3fffb2ddd4f4444a3aef7b8fa4812971fe340ea3
Derek Lorenzen <derek@digitalassetsdata.com>
2019-10-30 13:22:38 -0600

    'Updated readme. Added docstrings'

--- updating.py
+++ updating.py
@@ -4,16 +4,37 @@ import ipywidgets as widgets
 import asyncio
 
 class Updater(object):
+    '''Designed to initiate a streaming/updating task for a chart object. It repeatedly calls the plot
+    function, thus the plot coming in should represent the most recent data.
+    Params:
+        plot - plot(), Chart object, gets passed to IPython display. 
+        activated - Bool, whether the chart should initiate as streaming or static. 
+        rate - Int, slider value, for the starting delay in updating. 
+        **kwargs - Dict, passed to Output() object for displaying the chart. 
 
-    def __init__(self, plot, activated=False, rate=5, **kwargs):
-        self.out = widgets.Output()
+    Returns: 
+        Updater() - call to Updater().run() begins the procedure. 
+    '''
+
+    def __init__(self, plot, activated=False, rate=5, height='500px', **kwargs):
+        '''It is important to specify a height for the Output() object so that on update, the
+        IPython display need not rescale. This allows the chart to flicker but not change size. 
+        '''
+        self.out = widgets.Output(layout=widgets.Layout(border='1px solid grey',
+                                                        height=height,
+                                                        #justify_content='space_around',
+                                                        align_items='center',
+                                                        **kwargs))
         self.plot = plot()
-        self.activated = not activated
+        self.activated = not activated # Flip activated, to unflip and initialize later. 
         self.rate = rate
-        self.collection = self._construct(**kwargs)
+        self.collection = self._construct()
         
-    def _construct(self, **kwargs):
-        items_layout = widgets.Layout(width='70%')     # override the default width of the button to 'auto' to let the button grow
+    def _construct(self):
+        '''Construct the widget objects. Changes to appearance of the task bar above the plot
+        should go here.
+        '''
+        items_layout = widgets.Layout(width='70%')
 
         box_layout = widgets.Layout(display='flex',
                                     border='1px solid grey',
@@ -21,12 +42,15 @@ class Updater(object):
                                     width='100%')
 
         checkbox = widgets.Checkbox(value=self.activated, layout=items_layout, description='Update')
-        slider = widgets.IntSlider(min=1, max=60, step=1, value=self.rate, continuous_update=False, layout=items_layout, description='Delay', **kwargs)
+        slider = widgets.IntSlider(min=1, max=60, step=1, value=self.rate, continuous_update=False, layout=items_layout, description='Delay')
         collection = widgets.HBox([checkbox, slider], layout=box_layout)
         return collection
 
     def _main(self):
-        # Return widgets and the updated output widget
+        '''Update widgets.Output() display object.
+        Returns:
+            widgets collection - original widgets, updated plot. 
+        '''
         with self.out:
             self.out.clear_output(wait=True)
             # Plot should return chart object
@@ -35,6 +59,11 @@ class Updater(object):
 
     # Coroutines related
     def _on_check(self, c):
+        '''On widget state change, widget.observe() produces 3 dictionaries with matching keys. 
+        The first is the releasing of the widget resource and contains no pertinent information. 
+        The second contains the actual value change, and the third is the subsequent locking. 
+        On checkbox check, we initiate a coroutine to update repeatedly. 
+        '''
         if c['name'] == 'value' and c['old'] != c['new']:
             if c['new']:
                 asyncio.Task(self.update_loop())
@@ -42,18 +71,27 @@ class Updater(object):
                 asyncio.Task(self.update())
 
     async def update_loop(self):
+        '''Awaitable coroutine for the loop. `await` frees the kernel while it sleeps.
+        Method monitors for a change in the widget state to terminate.
+        '''
         while True:
-            print('updating')
             display(self._main())
             await asyncio.sleep(self.collection.children[1].value)
             if self.collection.children[0].value == False:
                 break
 
     async def update(self):
-        print('rendering')
+        '''Coroutine for static rendering of chart. Must be awaitable, or in other words, there
+        must be a release of the kernel, thus sleep(0) is used.
+        '''
         display(self._main())
-        await asyncio.sleep(0) # Free kernel
+        await asyncio.sleep(0)
 
     def run(self):
+        '''Begins widget based coroutine that monitors the widget state for changes. If the kernel
+        is freed, this task can resume allowing state updates within previously information-closed 
+        loops. Note: On observe, no change has occured to the widget. As a result we manually toggle 
+        the widget forcing the tasks to begin. Reversal of self.activated = not activated.
+        '''
         self.collection.children[0].observe(self._on_check)
         self.collection.children[0].value = not self.collection.children[0].value
